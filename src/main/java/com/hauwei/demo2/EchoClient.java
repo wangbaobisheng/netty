e30d7ff4c2b31a05279874aabbb6f4ed131a1f63
wangbaobisheng <wangbaobisheng@163.com>
2018-10-10 16:43:48 +0800

    按照字符解码

---
+++ src/main/java/com/hauwei/demo2/EchoClient.java
@@ -0,0 +1,73 @@
+package com.hauwei.demo2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class EchoClient {
+
+    public static void main(String[] args) throws Exception {
+        new EchoClient().connect(8080, "localhost");
+    }
+
+    public void connect(int port, String host) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ByteBuf delimter = Unpooled.copiedBuffer("$_".getBytes());
+                            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimter));
+                            socketChannel.pipeline().addLast(new StringEncoder());
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect(host, port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private class EchoClientHandler extends ChannelHandlerAdapter {
+        private int counter = 0;
+        String  ECHO_REQ = "Hi Lilinfeng, Welcome to Netty.$_";
+
+        /**
+         * 连通后发送消息给服务端
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            for (int i =0; i < 10; i++) {
+                ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+            }
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            System.out.println("This is " + ++counter + " times receive server : ["
+            + msg + "]");
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            ctx.close();
+        }
+    }
+}
---
+++ src/main/java/com/hauwei/demo2/EchoServer.java
@@ -0,0 +1,69 @@
+package com.hauwei.demo2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer().bind(8080);
+    }
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        public void initChannel(SocketChannel socketChannel) throws Exception {
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,delimiter));
+                            socketChannel.pipeline().addLast(new StringDecoder());
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class EchoServerHandler extends ChannelHandlerAdapter {
+        int countter = 0;
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            String body = (String)msg;
+            System.out.println("This is " + ++countter + " times receive client : ["
+            + body +"]");
+            body +="$_";
+            ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+            ctx.writeAndFlush(echo);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+            ctx.close();
+        }
+    }
+}
